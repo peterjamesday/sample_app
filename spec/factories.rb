bde22d85f3b8b4f378adb952cb9d0a0dbb161a8a
Peter Day <peter.day4@gmail.com>
2013-11-04 13:51:30 -0800

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Peter Day"
+    email    "peter.day4@gmail.com"
+    password "AnnaLivia!PT2"
+    password_confirmation "AnnaLivia!PT2"
+  end
+end
